e2e3535c939816f30392219050a7ac216ce5cc80
Robert Jones <60453163+rbjones92@users.noreply.github.com>
2022-08-16 11:31:43 -0700

    change reading text file locally to reading web

--- py_files/Wordle_Words.py
+++ py_files/Wordle_Words.py
@@ -1,13 +1,19 @@
 # Robert Jones
-# 8.10.22
-# Wordle Words
+# 8/16/22
+# Scraping a website to download wordle words
 
+import requests # For DLing HTML
+from bs4 import BeautifulSoup as bs # To work with HTML
+ 
 
 class WordleWords:
 
+    # function to parse website and return wordle words
     def get_words():
-
-        with open('C:/Users/Bob/Desktop/SpringBoard/Python_Projects/Wordle/words/wordle_words.txt') as f:
-            words = f.read().splitlines()
+        # Website with all 5 letter words
+        URL = 'https://raw.githubusercontent.com/tabatkins/wordle-list/main/words'
+        words = str(bs(requests.get(URL).text, 'html.parser'))
+        words = words.split()
         return words
+
 
